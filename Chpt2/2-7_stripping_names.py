f79e3c479b24219d94683c2e3aac9c482b3e24db
reubadoob <31323201+reubadoob@users.noreply.github.com>
2022-12-28 22:14:45 -0500

    Update 2-7_stripping_names.py
    Anonymized data

--- Chpt2/2-7_stripping_names.py
+++ Chpt2/2-7_stripping_names.py
@@ -1,5 +1,5 @@
-first_name = " Reuben "
-last_name = " Booker "
+first_name = " Mike "
+last_name = " Jones "
 full_name = f"{first_name} {last_name}"
 print(full_name)
 print(f"\nFIRST NAME:{first_name.lstrip()} \n\tLAST NAME:{last_name.lstrip()}")
