fb750a067811a1459b6ff1cb7a1f6ef7f419ec74
reuben.booker@appdynamics.com <reuben.booker@appdynamics.com>
2022-12-29 10:35:07 -0500

    Completed exercise

--- Chpt6/6-6_Polling.py
+++ Chpt6/6-6_Polling.py
@@ -1,7 +1,18 @@
+# The code below is used as the basis for the solution. It found on page 97
 favorite_languages = {
     'jen': 'python',
     'sarah': 'c',
     'edward': 'ruby',
     'phil': 'python',
 }
-language = favorite_languages
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
+
+# The solution for this exercise is below
+voters = ['jen', 'sarah', 'edward', 'phil', 'alice', 'bob', 'charlie']
+for person in voters:
+    print("Hello " + person.title() + "!")
+    if person in (favorite_languages.keys()):
+        print(f"{person.title()}, thank you for taking the poll.")
+    else:
+        print(f"{person.title()}, please take our poll!")
