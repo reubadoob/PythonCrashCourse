aa9d6f662ab554db64db4152aad7f47cf13d0069
reuben.booker@appdynamics.com <reuben.booker@appdynamics.com>
2023-01-09 14:14:47 -0500

    Completed exercise

---
+++ Chpt6/6-7_People.py
@@ -0,0 +1,13 @@
+person_0 = {'first_name': 'Mike', 'last_name': 'Jones', 'birth_state': 'Texas',
+            'birth_city': 'Houston', 'age': '37', 'phone_number': '281-330-8004'}
+
+person_1 = {'first_name': 'Mary', 'last_name': 'Jones', 'birth_state': 'Texas',
+            'birth_city': 'Houston', 'age': '38', 'phone_number': '281-330-8005'}
+
+person_2 = {'first_name': 'Martha', 'last_name': 'Jones', 'birth_state': 'Texas',
+            'birth_city': 'Houston', 'age': '39', 'phone_number': '281-330-8006'}
+
+people = [person_0, person_1, person_2]
+
+for person in people:
+    print(person)
