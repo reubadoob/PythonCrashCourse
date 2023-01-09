083a101cc7f27b7fe307499e0f6c46fc0e56ef10
reuben.booker@appdynamics.com <reuben.booker@appdynamics.com>
2023-01-09 14:29:41 -0500

    Completed exercise

---
+++ Chpt6/6-8_Pets.py
@@ -0,0 +1,10 @@
+pet_0 = {'name': 'Jake', 'pet_type': 'dog', 'breed': 'Retriever', 'owner_name': 'Alice'}
+pet_1 = {'name': 'Charlie', 'pet_type': 'dog', 'breed': 'English Bulldog', 'owner_name': 'Bob'}
+pet_2 = {'name': 'Berringer', 'pet_type': 'dog', 'breed': 'Mutt', 'owner_name': 'Chuck'}
+pet_3 = {'name': 'Tux', 'pet_type': 'dog', 'breed': 'Schnauzer', 'owner_name': 'Dave'}
+pet_4 = {'name': 'Gunner', 'pet_type': 'dog', 'breed': 'Lab', 'owner_name': 'Eric'}
+
+pets = [pet_4, pet_3, pet_2, pet_1, pet_0]
+
+for pet in pets:
+    print(pet)
