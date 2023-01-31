bbf9018a63d59778cc7dec2f0edce4bed47b0eb9
reuben.booker@appdynamics.com <reuben.booker@appdynamics.com>
2023-01-31 14:53:21 -0500

    Initial commit - Exercises Complete

---
+++ Chpt7/7-10_Dream_Vacation.py
@@ -0,0 +1,9 @@
+# Dream Vacation Survey
+
+print("Welcome to the Dream Vacation Survey!")
+
+# Ask the user to input their dream vacation
+vacation = input("If you could visit one place in the world, where would you go? ")
+
+# Print the results of the poll
+print("\nGreat! So your dream vacation is " + vacation + ". Thank you for participating in the survey!")
